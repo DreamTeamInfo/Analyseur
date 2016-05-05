8adc6dedbe6b35ddf3778255d4478d4313d38b90
FrenchMamba <maxime.baube@outlook.fr>
2016-05-05 21:56:39 +0200

    Added files via upload

---
+++ Methode.java
@@ -0,0 +1,16 @@
+/**
+ * Methode.java    29/04/2016
+ * IUT INFO1 2015/2016
+ */
+package iut.info1.methode;
+
+/**
+ * On cherche ici à décrire tous les éléments des méthodes
+ * d'une classe particulière, comme par exemple les champs, les paramètres...etc
+ * @author maxime
+ * @version 0.1
+ */
+public class Methode {
+
+	/** */
+}
